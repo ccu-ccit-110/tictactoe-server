2e36735227efa3171d6c365aa97ca2561aac924a
luckstar77 <luckstar77y@gmail.com>
2021-12-24 21:32:40 +0800

    feat: leave

--- index.js
+++ index.js
@@ -38,11 +38,10 @@ io.on("connection", (socket) => {
       if(!rooms[roomname]) return socket.emit('error', `你沒有加入"${roomname}"房間`);
       socket.leave(roomname)
       socket.emit('leave', {username:socket.username});
-      socket.status = 'free';
       if(rooms[roomname].count === 1 || socket.status === 'host') {
         delete rooms[roomname];
-        io.in(roomname).socketsLeave(roomname);
         socket.to(roomname).emit('leave', {username:socket.username});
+        io.in(roomname).socketsLeave(roomname);
       } else {
         rooms[roomname] = {
           ...rooms[roomname],
@@ -52,6 +51,7 @@ io.on("connection", (socket) => {
         }
         socket.to(roomname).emit('restart', rooms[roomname]);
       }
+      socket.status = 'free';
     });
 
     socket.on("disconnect", (reason) => {
@@ -59,11 +59,10 @@ io.on("connection", (socket) => {
       if(!rooms[roomname]) return socket.emit('error', `你沒有加入"${roomname}"房間`);
       socket.leave(roomname)
       socket.emit('leave', {username:socket.username});
-      socket.status = 'free';
       if(rooms[roomname].count === 1 || socket.status === 'host') {
         delete rooms[roomname];
-        io.in(roomname).socketsLeave(roomname);
         socket.to(roomname).emit('leave', {username:socket.username});
+        io.in(roomname).socketsLeave(roomname);
       } else {
         rooms[roomname] = { 
           ...rooms[roomname],
@@ -73,6 +72,7 @@ io.on("connection", (socket) => {
         }
         socket.to(roomname).emit('restart', rooms[roomname]);
       }
+      socket.status = 'free';
     });
 });
 
