1e7fbac1c1506dc84393515ebca391f2bb6d069b
luckstar77 <luckstar77y@gmail.com>
2021-12-24 22:57:42 +0800

    feat: end

--- index.js
+++ index.js
@@ -15,15 +15,26 @@ io.on("connection", (socket) => {
       if(rooms[roomname]) return socket.emit('error', '房間名稱重複');
       socket.username = username;
       socket.join(roomname)
-      rooms[roomname] = {count: 1, step:[], host: username, guest: '', roomname, who:'host'};
+      rooms[roomname] = {count: 1, step:{}, host: username, guest: '', roomname, who:'host'};
       socket.status = 'host';
       socket.roomname = roomname;
       socket.emit('create', rooms[roomname]);
     });
 
+    socket.on('restart', ({username, roomname}) => {
+      if(!rooms[roomname]) return socket.emit('error', '房間尚未建立');
+      if(rooms[roomname].count !== 2) return socket.emit('error', '房間尚未開始對弈');
+      if(!rooms[roomname].end) return socket.emit('error', '對弈尚未結束');
+      rooms[roomname].end = undefined;
+      rooms[roomname].who = 'host';
+      rooms[roomname].step = {};
+      socket.emit('restart', rooms[roomname])
+      socket.to(roomname).emit('restart', rooms[roomname]);
+    });
+
     socket.on('join', ({username, roomname}) => {
       if(!rooms[roomname]) return socket.emit('error', '房間尚未建立');
-      if(rooms[roomname].count === 2) return socket.emit('error', '房間已開始對戰');
+      if(rooms[roomname].count === 2) return socket.emit('error', '房間已開始對弈');
       if(rooms[roomname].host === username) return socket.emit('error', '名稱與房主重複');
       socket.username = username;
       socket.join(roomname)
@@ -33,6 +44,38 @@ io.on("connection", (socket) => {
       socket.roomname = roomname;
       socket.emit('join', rooms[roomname]);
       socket.to(roomname).emit('step', rooms[roomname]);
+    });
+
+    socket.on('step', ({who, index}) => {
+      const roomname = socket.roomname;
+      if(!rooms[roomname]) return socket.emit('error', '房間尚未建立');
+      if(rooms[roomname].count !== 2) return socket.emit('error', '房間尚未開始對弈');
+      if(rooms[roomname].end) return socket.emit('error', '棋局已經結束');
+      if(rooms[roomname].step[index]) return socket.emit('error', '此步已被下子');
+      rooms[roomname].step[index] = {
+        who,
+      }
+      rooms[roomname].who = who === 'host' ? 'guest' : 'host';
+      socket.emit('step', rooms[roomname]);
+      socket.to(roomname).emit('step', rooms[roomname]);
+      if(Object.keys(rooms[roomname].step).length > 4) {
+        const check_end = (end) => {
+          if(rooms[roomname].step[0]?.who === end && rooms[roomname].step[1]?.who === end && rooms[roomname].step[2]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[3]?.who === end && rooms[roomname].step[4]?.who === end && rooms[roomname].step[5]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[6]?.who === end && rooms[roomname].step[7]?.who === end && rooms[roomname].step[8]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[0]?.who === end && rooms[roomname].step[3]?.who === end && rooms[roomname].step[6]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[1]?.who === end && rooms[roomname].step[4]?.who === end && rooms[roomname].step[7]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[2]?.who === end && rooms[roomname].step[5]?.who === end && rooms[roomname].step[8]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[0]?.who === end && rooms[roomname].step[4]?.who === end && rooms[roomname].step[8]?.who === end) rooms[roomname].end = end;
+          if(rooms[roomname].step[2]?.who === end && rooms[roomname].step[4]?.who === end && rooms[roomname].step[6]?.who === end) rooms[roomname].end = end;
+        }
+        
+        check_end(who);
+        if(rooms[roomname].end) {
+          socket.emit('end', rooms[roomname]);
+          socket.to(roomname).emit('end', rooms[roomname]);
+        }
+      }
     });
 
     socket.on('leave', roomname => {
@@ -47,7 +90,7 @@ io.on("connection", (socket) => {
         rooms[roomname] = {
           ...rooms[roomname],
           count: 1,
-          step:[],
+          step:{},
           guest: '',
         }
         socket.to(roomname).emit('restart', rooms[roomname]);
@@ -68,7 +111,7 @@ io.on("connection", (socket) => {
         rooms[roomname] = { 
           ...rooms[roomname],
           count: 1,
-          step:[],
+          step:{},
           guest: '',
         }
         socket.to(roomname).emit('restart', rooms[roomname]);
